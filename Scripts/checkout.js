0d35cac78c8b56fde2f7c6b02b0b8e0fc14b6dc7
DARKVIEWNERD <vincentnarvaez391@gmail.com>
2024-10-15 19:55:25 +0800

    save data and generate html for payment summary

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderHtml } from "./Checkout/orderSummaryCheckout.js";
+import { renderpaymentSummary } from "./Checkout/paymentSummaryCheckout.js";
 
 renderOrderHtml();
+renderpaymentSummary();
