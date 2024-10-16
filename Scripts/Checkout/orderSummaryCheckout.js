9ed7801bc54106568451292473a95b812d9b2037
DARKVIEWNERD <vincentnarvaez391@gmail.com>
2024-10-16 19:26:11 +0800

    normalize the dayjs by making it function

--- Scripts/Checkout/orderSummaryCheckout.js
+++ Scripts/Checkout/orderSummaryCheckout.js
@@ -2,7 +2,7 @@ import { cart,removeFromCart,updateDeliveryOption } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { formatCurrency } from "..//utils/currency.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import {deliveryOptions, getDeliveryOption} from "../../data/DeliveryOption.js";
+import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from "../../data/DeliveryOption.js";
 import { renderpaymentSummary } from "./paymentSummaryCheckout.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js"; 
 
@@ -12,21 +12,12 @@ export function renderOrderHtml(){
     const productId=cartItem.productId;
 
     const matchingProduct=getProduct(productId);
-
  
-  const deliveryOptionId=cartItem.deliveryOptionId;
+    const deliveryOptionId=cartItem.deliveryOptionId;
 
-  const deliveryOption=getDeliveryOption(deliveryOptionId);
+    const deliveryOption=getDeliveryOption(deliveryOptionId);
 
-  const today=dayjs();
-
-  const deliveryDate=today.add(
-    deliveryOption.deliveryDays,'days' 
-  );
-
-  const dateString=deliveryDate.format(
-    'dddd, MMMM, D'
-  );
+    const dateString=calculateDeliveryDate(deliveryOption);
     cartsummaryHTML+=`
     <div class="cart-item-container
        js-cart-item-container-${matchingProduct.id}">
@@ -72,13 +63,9 @@ export function renderOrderHtml(){
  function deliveryOptionhtml(matchingProduct,cartItem){
   let html=''
   deliveryOptions.forEach((deliveryOption)=>{
-    const today=dayjs();
-    const deliveryDate=today.add(
-      deliveryOption.deliveryDays,'days' 
-    );
-    const dateString=deliveryDate.format(
-      'dddd, MMMM, D'
-    );
+    
+    const dateString=calculateDeliveryDate(deliveryOption);
+  
     const priceString=deliveryOption.priceCents===0
     ?'FREE'
     : `$${formatCurrency(deliveryOption.priceCents)}`;
