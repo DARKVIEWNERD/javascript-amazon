cd64018d849fb89c6b64d273c63028fc29b09e87
DARKVIEWNERD <vincentnarvaez391@gmail.com>
2024-10-18 09:15:14 +0800

    make localstorage and load from page private using #

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,7 +1,8 @@
 import { renderOrderHtml } from "./Checkout/orderSummaryCheckout.js";
 import { renderpaymentSummary } from "./Checkout/paymentSummaryCheckout.js";
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";  
-import  '../data/cart-oop.js';
+import  '../data/cart-class.js';
+
 renderOrderHtml();
 renderpaymentSummary();
 renderCheckoutHeader();
